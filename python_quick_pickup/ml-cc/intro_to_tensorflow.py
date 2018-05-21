a15d7ba1818c88e13bb4ada8837a9ff0572dda5b
haominshi <haominshi@gmail.com>
2018-05-20 21:43:16 -0500

    added

--- python_quick_pickup/ml-cc/intro_to_tensorflow.py
+++ python_quick_pickup/ml-cc/intro_to_tensorflow.py
@@ -26,3 +26,22 @@ print(california_housing_dataframe)
 # after we get our hands on the data we can exam it
 # this is the pandas function we learned previously
 print(california_housing_dataframe.describe())
+
+## The building of the first model
+## The feature column
+# define the input feature: total room
+my_feature = california_housing_dataframe[['total_rooms']]
+# configure a numeric feature column for total_rooms
+feature_columns = [tf.feature_column.numeric_column("total_rooms")]
+
+## now we define the target
+# define the label 
+target = california_housing_dataframe["median_house_value"]
+
+# Use gradient descent as the optimizer for training
+my_optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.0000001)
+my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer,5.0)
+
+# Configure the linear regression model with our feature columns and optimizer
+# set a learning rate of 0.0000001 for the gradient desent
+linear_regressor = tf.estimator.LinearRegressor(feature_columns=feature_columns,optimizer=my_optimizer)
