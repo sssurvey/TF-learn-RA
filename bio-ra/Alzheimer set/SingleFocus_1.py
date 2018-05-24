483c029951e3992df5b6d33c871155206d415d5d
haominshi <haominshi@gmail.com>
2018-05-24 16:58:27 -0500

    added

--- bio-ra/Alzheimer set/SingleFocus_1.py
+++ bio-ra/Alzheimer set/SingleFocus_1.py
@@ -13,26 +13,43 @@ def openExcelSheet(path, sheet_name):
     return dataframe_excel
 
 def analysis_1(df):
-    """this is a time based analyis to see what changed during the visit"""
     tmp_df = df[df.columns[5:89]]
     print (tmp_df)
     plt.figure(); tmp_df.plot()
     plt.show()
 
 def analysis_2(df):
-    """find corralations"""
-    for i in range(5,90):
+    for i in range(5,89):
         print (df[df.columns[i]])
         temp_df = df[df.columns[i]]
         plt.figure(); temp_df.plot()
     plt.show()
+
+def analysis_3(df):
+    temp_df = df.copy()
+    column_names = list(temp_df.columns.values)
+    monitor_dict = {}
+    for i in range(4,89):
+        change = float(temp_df.iloc[-1,i]) - float(temp_df.iloc[0,i])
+        # need to deal with NaN
+        # print(change)
+        monitor_dict[i] = change
+    return monitor_dict
+
         
 
 
 def main():
-    df_1 = openExcelSheet('/Users/haominshi/Desktop/al_data/individual.xlsx',sheet_name="Sheet1")
-    analysis_1(df_1)
-    analysis_2(df_1)
+    df_1 = openExcelSheet(
+        '/Users/haominshi/Desktop/al_data/individual.xlsx',sheet_name="Sheet1")
+    df_2 = openExcelSheet(
+        '/Users/haominshi/Desktop/al_data/individual.xlsx', sheet_name="Sheet2")
+    # analysis_1(df_1)
+    df_dict_1 = analysis_3(df_1)
+    print (df_dict_1)
+    df_dict_2 = analysis_3(df_2)
+    print (df_dict_2)
+    # analysis_2(df_1)
 
 if __name__ == "__main__":
     main()
