81bb19fdf04ed12d50d511f2130bc1d6c609147b
haominshi <haominshi@gmail.com>
2018-05-28 17:07:10 -0500

    created

---
+++ bio-ra/Alzheimer set/Grouper.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+# import seaborn as sns
+import matplotlib.pyplot as plt
+import pylab
+# import openpyxl as oxl
+import SingleFocus_1 as sf1
+
+'''
+the purpose of this file is to scan the csv
+then group the subject based on their ID number
+create a list of df that each df is one patient
+with 2 group:
+    1. was diagnosed
+    2. was not diagnosed
+'''
+
+
+def main():
+    path = '/Users/haominshi/Desktop/al_data/dataset_576_long.xlsx'
+    data_set_everything = sf1.openExcelSheet(path, sheet_name="Sheet1")
+    # file transfered to DF
+    
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
